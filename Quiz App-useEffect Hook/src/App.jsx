a7f1c6d0e73d5d0a86db1b49b298830bf571c1ad
Musab Bin Ubaid <joiya4636@gmail.com>
2024-09-01 22:21:46 -0700

    quiz app component added

--- Quiz App-useEffect Hook/src/App.jsx
+++ Quiz App-useEffect Hook/src/App.jsx
@@ -1,3 +1,10 @@
-function App() {}
+import Header from "./components/header";
+
+
+function App() {
+    return (
+<Header></Header>
+    );
+}
 
 export default App;
--- Quiz App-useEffect Hook/src/components/quiz.jsx
+++ Quiz App-useEffect Hook/src/components/quiz.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Quiz(props) {
-    
+   const [Question,setQuestion]= useState(0);
+   const [answers,setAnswers]=useState([]);
 
     return (
         <>
-            
+            <p>
+                Active Question
+            </p>
         </>
     )
 }
