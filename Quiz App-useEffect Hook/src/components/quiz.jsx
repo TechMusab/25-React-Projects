a6090cd324bf32cab4f9f86449d94e594f108893
Musab Bin Ubaid <joiya4636@gmail.com>
2024-09-02 11:46:32 -0700

    Update quiz.jsx

--- Quiz App-useEffect Hook/src/components/quiz.jsx
+++ Quiz App-useEffect Hook/src/components/quiz.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Quiz(props) {
-   const [Question,setQuestion]= useState(0);
-   const [answers,setAnswers]=useState([]);
+  const [Question, setQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
 
-    return (
-        <>
-            <p>
-                Active Question
-            </p>
-        </>
-    )
+  return (
+    <>
+      <p>Active Question</p>
+      <div></div>
+    </>
+  );
 }
