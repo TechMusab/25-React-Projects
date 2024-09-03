49ed4625f4135f4f8b6cb16507accebe6002ea4e
Musab Bin Ubaid <joiya4636@gmail.com>
2024-09-03 10:45:40 -0700

    updated Quiz Component

--- Quiz App-useEffect Hook/src/App.jsx
+++ Quiz App-useEffect Hook/src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/header";
-
+import Quiz from "./components/quiz";
 
 function App() {
-    return (
-<Header></Header>
-    );
+  return (
+    <>
+      <Header></Header>
+      <Quiz></Quiz>
+    </>
+  );
 }
 
 export default App;
--- Quiz App-useEffect Hook/src/components/quiz.jsx
+++ Quiz App-useEffect Hook/src/components/quiz.jsx
@@ -3,11 +3,26 @@ import QUESTIONS from "../../questioons";
 
 export default function Quiz(props) {
   const [answers, setAnswers] = useState([]);
+  function handleselectanswer(index) {
+    console.log(index);
+   setAnswers((prev) => [...prev,index]);
+  }
   const questionindex = answers.length;
 
   return (
     <div id="question">
       <h2>{QUESTIONS[questionindex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[questionindex].answers.map((answer, index) => {
+          return (
+            <li className="answer" key={answer}>
+              <button onClick={() => handleselectanswer(index)}>
+                {answer}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
