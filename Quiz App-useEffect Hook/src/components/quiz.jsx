3c1a0e4b89eb969bc900001fd521a210ded8c724
Musab Bin Ubaid <joiya4636@gmail.com>
2024-09-03 11:12:54 -0700

    Update quiz.jsx

--- Quiz App-useEffect Hook/src/components/quiz.jsx
+++ Quiz App-useEffect Hook/src/components/quiz.jsx
@@ -1,30 +1,39 @@
 import React, { useState } from "react";
 import QUESTIONS from "../../questioons";
-
+import quizcompleteimg from "../assets/quiz-complete.png";
 export default function Quiz(props) {
   const [answers, setAnswers] = useState([]);
-  function handleselectanswer(index) {
-    console.log(index);
-   setAnswers((prev) => [...prev,index]);
+  function handleselectanswer(answer) {
+    setAnswers((prev) => [...prev, answer]);
+  }
+  if (answers.length === QUESTIONS.length) {
+    return (
+      <div id="summary">
+        <h2>Quiz Completed</h2>
+        <img src={quizcompleteimg} alt="Complete image" />
+      </div>
+    );
   }
   const questionindex = answers.length;
+  const shuffledanswers = [...QUESTIONS[questionindex].answers];
+  shuffledanswers.sort((a, b) => Math.random() - 0.5);
 
   return (
     <div id="quiz">
-    <div id="question">
-      <h2>{QUESTIONS[questionindex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[questionindex].answers.map((answer, index) => {
-          return (
-            <li className="answer" key={answer}>
-              <button onClick={() => handleselectanswer(index)}>
-                {answer}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+      <div id="question">
+        <h2>{QUESTIONS[questionindex].text}</h2>
+        <ul id="answers">
+          {shuffledanswers.map((answer) => {
+            return (
+              <li className="answer" key={answer}>
+                <button onClick={() => handleselectanswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
