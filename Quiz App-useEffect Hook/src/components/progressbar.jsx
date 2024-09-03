71b557b617b0ce604cd6ce4427889c8340726573
Musab Bin Ubaid <joiya4636@gmail.com>
2024-09-03 12:30:59 -0700

    Update progressbar.jsx

--- Quiz App-useEffect Hook/src/components/progressbar.jsx
+++ Quiz App-useEffect Hook/src/components/progressbar.jsx
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 export default function Progressbar({ addanswer }) {
   const [remainingtime, setRemainingtime] = useState(5000);
   useEffect(() => {
+    console.log("Set intervel")
     const intervel = setInterval(() => {
       setRemainingtime((prev) => prev - 100);
     }, 100);
-    return () => clearInterval(intervel);
   }, []);
   useEffect(() => {
+    console.log("Set Timeout")
     const timer = setTimeout(addanswer, 5000);
-    return () => clearTimeout(timer);
-  }, []);
+  }, [addanswer]);
 
   return (
     <>
