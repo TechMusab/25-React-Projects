22e21c7fd55af1a689f9cdac13125e4026262459
Musab Bin Ubaid <joiya4636@gmail.com>
2024-09-07 11:22:11 -0700

    updated

--- Quiz App-useEffect Hook/src/components/header.jsx
+++ Quiz App-useEffect Hook/src/components/header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import logoimg from '../assets/quiz-logo.png';
+import logoimg from "../assets/quiz-logo.png";
 
 export default function Header() {
-  return(
-<header>
-  <img src={logoimg} alt="quizlogo" />
-  <h1>Quiz App</h1>
-</header>
+  return (
+    <header>
+      <img src={logoimg} alt="quizlogo" />
+      <h1>Quiz App</h1>
+    </header>
   );
 }
